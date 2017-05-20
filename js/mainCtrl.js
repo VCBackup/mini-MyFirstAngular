dfa4fa2bc7a660863295d3c627271f91dff3f8fe
Moore <DMoore@isolvedhcm.com>
2017-05-20 11:43:29 -0600

    Completed assignment with button and input field to add new friends.Though there is a bug with hitting the delete key breaking the functionality.

---
+++ js/mainCtrl.js
@@ -0,0 +1,10 @@
+angular.module('friendsList');
+
+angular.module('friendsList').controller('mainCtrl', function($scope){
+    $scope.danFriendsList = ['Josh', 'Dallin', 'Makenzie', 'Robyn'];
+    function addFriend(){
+        var jsFriendBox = ('friendBox');
+        danFriendsList = danFriendsList + $scope.danFriendsList.push(jsFriendBox.html().val());
+    }
+
+});
